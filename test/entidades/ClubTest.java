4a919f364321729c1a24efb864824799e1ca3983
Paco Lopez <spanocrypt@gmail.com>
2025-03-24 14:06:53 +0100

    Correccion errores JUnit5 para tests unitarios

--- test/entidades/ClubTest.java
+++ test/entidades/ClubTest.java
@@ -1,11 +1,8 @@
 package entidades;
 
 import java.time.LocalDate;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClubTest {
 
