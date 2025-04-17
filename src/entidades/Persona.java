b6e9c4b98295b4dea1ba18b7a38cc965c002775f
David0529326 <david.ortiz051205@gmail.com>
2025-04-17 20:25:25 +0200

    modificaciones añadidas
    Corregida la consulta SQL, modificado los valores para que compruebe y se meta en la base de datos para consultar informacion en metodo buscapersonas y añadir try catch en los metodos de nuevapersona y buscapersona

---
+++ src/entidades/Persona.java
@@ -0,0 +1,217 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entidades;
+import proyectoffcv.util.DatabaseConnection;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.*;
+/**
+ *
+ * @author david
+ */
+public class Persona {
+   
+ private String DNI, nombre, apellido1, apellido2, usuario, password, poblacion;
+ private LocalDate fechaNacimiento;
+ private static ArrayList<Persona> personas;
+        
+  public Persona(String DNI, String nombre, String apellido1, String apellido2, String usuario, String password, String poblacion, LocalDate fechanacimiento){
+  this.DNI = DNI;
+  this.apellido1 = apellido1;
+  this.apellido2 = apellido2;
+  this.fechaNacimiento = fechanacimiento;
+  this.nombre = nombre;
+  this.password = password;
+  this.poblacion = poblacion;
+  this.usuario = usuario;
+  this.personas = new ArrayList<>();
+  }      
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setDNI(String DNI) {
+        
+    if(DNI == null || DNI.trim().isEmpty()){
+        System.out.println("El campo esta vacío o es nulo");
+    }    
+      this.DNI = DNI;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public static ArrayList<Persona> getPersonas() {
+        return personas;
+    }
+
+    public static void setPersonas(ArrayList<Persona> personas) {
+        Persona.personas = personas;
+    }
+    
+    
+    
+    
+    
+    public static Persona nuevaPersona(String dni, String nombre, String apellido1, String apellido2, LocalDate fechaNacimiento, String usuario, String password, String poblacion) {
+    try {
+        
+    if (buscaPersona(dni) == null) {
+        Persona persona = new Persona(dni, nombre, apellido1, apellido2, usuario, password, poblacion, fechaNacimiento);
+        
+        personas.add(persona);
+        return persona;
+    
+    } else {
+        
+        System.out.println("La persona con DNI " + dni + " ya existe.");
+    }
+    
+    }catch (Exception e) {
+        
+        System.err.println("Error al crear nueva persona");
+    }
+
+    return null;
+    }
+    
+    public static Persona buscaPersona(String dni){
+        
+        for (int i = 0; i < personas.size(); i++){
+            
+            if (personas.get(i).getDNI().equals(dni)) {
+                return personas.get(i);
+            }
+        }
+        
+        String consulta = "SELECT * FROM persona WHERE dni = ?";
+    try (Connection conn = DatabaseConnection.getConnection();
+         PreparedStatement st = conn.prepareStatement(consulta)) {
+
+        st.setString(1, dni);
+        ResultSet rs = st.executeQuery();
+
+        if (rs.next()) {
+            Persona persona = new Persona(
+                rs.getString("dni"),
+                rs.getString("nombre"),
+                rs.getString("apellido1"),
+                rs.getString("apellido2"),
+                rs.getString("usuario"),
+                rs.getString("password"),
+                rs.getString("poblacion"),
+                rs.getDate("fechaNacimiento").toLocalDate()
+            );
+            personas.add(persona);
+            return persona;
+        }
+
+    } catch (SQLException e) {
+        System.out.println("Error al acceder a la base de datos");
+    }
+        return null;
+    }
+    
+     public static ArrayList<Persona> buscaPersonas(String nombre, String apellido1, String apellido2) {
+        ArrayList<Persona> busqueda = new ArrayList<>();
+        for (int i = 0; i < personas.size(); i++) {
+            Persona persona = personas.get(i);
+            if (persona.getNombre() != null && persona.getNombre().equals(nombre) && persona.getApellido1() != null && persona.getApellido1().equals(apellido1) && persona.getApellido2() != null && persona.getApellido2().equals(apellido2))
+            {
+                busqueda.add(persona);
+            }
+            }
+            return busqueda;
+        }
+        
+    
+     
+     public void Persistencia()throws SQLException{
+     
+     String consulta = "INSERT INTO persona (dni, apellido1, apellido2, fechaNacimiento, usuario, password, poblacion)"+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+     
+    try(Connection conn = DatabaseConnection.getConnection();
+    
+     PreparedStatement valor = conn.prepareStatement(consulta)){
+     
+     valor.setString(1, DNI);       
+     valor.setString(2, nombre);        
+     valor.setString(3, apellido1);
+     valor.setString(4, apellido2);
+     valor.setDate(5, java.sql.Date.valueOf(fechaNacimiento));
+     valor.setString(6, usuario);
+     valor.setString(7, password);
+     valor.setString(8, poblacion);
+    
+     valor.executeUpdate();
+    }
+    }
+    
+   @Override
+    public String toString() {
+        return "Nombre: "+ nombre + "\n Primer Apellido: " + apellido1 + "\n Segundo Apellido: " + apellido2 + "\n DNI: " + DNI + "\n Fecha de nacimiento: "+ fechaNacimiento +"\n Poblacion: "+ poblacion +"\n Usuario: "+ usuario +"\n Contraseña: "+ password;
+    }   
+        
+   
+   
+   
+    
+    
+}
