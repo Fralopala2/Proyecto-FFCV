99c3acc2fb9678761e532d038a58f061a9ba1212
Paco Lopez Alarte <spanocrypt@gmail.com>
2026-02-05 10:50:03 +0100

    Update installServerAws.sh

--- installServerAws.sh
+++ installServerAws.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://raw.githubusercontent.com/Fralopala2/Proyecto-FFCV/refs/heads/entrega_final/installServerAws.sh
+
 set -e
 
 echo "=== Script instalacion automatica AWS ==="
